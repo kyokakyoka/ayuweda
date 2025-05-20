58b4ee2c906e52f8bb0701c1ddf8ddbe02d95f90
kyoka-hidaka <hidakakyoka.0404@gmail.com>
2025-05-20 12:13:07 +0900

    front-page #3

--- wp-content/themes/ayuweda/front-page.php
+++ wp-content/themes/ayuweda/front-page.php
@@ -3,188 +3,187 @@
 <body>
     <main>
         <article>
-            <header>
-                <div>
-                    <h2>私が自分のお医者さま</h2>
-                    <p>古来からの贈り物アーユルヴェーダで人生をクリエイト</p>
+            <header id="articleHeader" class="l-articleHeader">
+                <div class="l-articleHeader-catchCopy">
+                    <h2 class="l-articleHeader-catchCopy-mainTxt">私が自分のお医者さま</h2>
+                    <p class="l-articleHeader-catchCopy-txt">古来からの贈り物アーユルヴェーダで人生をクリエイト</p>
                 </div>
-                <a href="">ご予約はこちら</a>
+                <a class="l-articleHeader-btn" href="">ご予約はこちら</a>
+                <img src="assets/images/top/mainVisual.MP4" alt="">
             </header>
-            <section id="about">
-                <h3>アーユルヴェーダで、<br>あなただけの特別なケアを。</h3>
-                <div>
-                    <p>アーユルヴェーダとは、5000年以上の歴史を持つインドやスリランカの伝統的な医学です。<br>心・身体・魂を整え、本来の健やかさと自分らしいさを取り戻していくための智慧。</p>
-                    <p>私たち一人ひとりには、体質を構成するドーシャ（エネルギーの要素）があり、<br>そのバランスは日々の暮らしや季節、心の状態によって少しずつ変化しています。<br>だからこそ、自分の身体と心の声に耳を傾け、今の自分に必要なケアを知ることが大切です。</p>
-                    <p>毎日の食事や瞑想、ヨガを取り入れたセルフケア、<br>アーユルヴェーダの施術やアストロロジーを活用することで、季節や月、惑星の流れを味方にしながら、<br>あなたの内側から調和を取り戻すお手伝いをいたします。</p>
-                    <p>今ここにいる自分を大切に。<br>静かに自分自身と向き合い、心と身体のバランスを整えていきませんか？</p>
+            <section id="about" class="l-about">
+                <h3 class="l-about-catchCopy">アーユルヴェーダで、<br>あなただけの特別なケアを。</h3>
+                <div class="l-about-txt">
+                    <p class="l-about-txt-item">アーユルヴェーダとは、5000年以上の歴史を持つインドやスリランカの伝統的な医学です。<br>心・身体・魂を整え、本来の健やかさと自分らしいさを取り戻していくための智慧。</p>
+                    <p class="l-about-txt-item">私たち一人ひとりには、体質を構成するドーシャ（エネルギーの要素）があり、<br>そのバランスは日々の暮らしや季節、心の状態によって少しずつ変化しています。<br>だからこそ、自分の身体と心の声に耳を傾け、今の自分に必要なケアを知ることが大切です。</p>
+                    <p class="l-about-txt-item">毎日の食事や瞑想、ヨガを取り入れたセルフケア、<br>アーユルヴェーダの施術やアストロロジーを活用することで、季節や月、惑星の流れを味方にしながら、<br>あなたの内側から調和を取り戻すお手伝いをいたします。</p>
+                    <p class="l-about-txt-item">今ここにいる自分を大切に。<br>静かに自分自身と向き合い、心と身体のバランスを整えていきませんか？</p>
                 </div>
-                <a href="">アーユルヴェーダについて詳しくみる</a>
+                <a class="l-about-btn" href="">アーユルヴェーダについて詳しくみる</a>
             </section>
-            <section id="ayurveda">
-                <h4>さまざまな角度から行うヘルスケア</h4>
-                <div class="diagram">
-                    <div class="center">
-                        <img src="woman.png" alt="女性のイラスト" />
+            <section id="ayurveda" class="l-ayurveda">
+                <h4 class="title">さまざまな角度から行うヘルスケア</h4>
+                <div class="l-ayurveda-diagram">
+                    <div class="l-ayurveda-diagram-center">
+                        <img src="woman.png" alt="女性のイラスト">
                     </div>
-                    <div class="circle-item item1">トリートメント</div>
-                    <div class="circle-item item2">食事</div>
-                    <div class="circle-item item3">宝石</div>
-                    <div class="circle-item item4">占星術</div>
-                    <div class="circle-item item5">ヨガ・瞑想</div>
-                    <div>
-                        <p>ピッタ</p>
-                        <p>PITTA</p>
+                    <div class="l-ayurveda-diagram-item item1">トリートメント</div>
+                    <div class="l-dayurveda-iagram-item item2">食事</div>
+                    <div class="l-ayurveda-diagram-item item3">宝石</div>
+                    <div class="l-ayurveda-diagram-item item4">占星術</div>
+                    <div class="l-ayurveda-diagram-item item5">ヨガ・瞑想</div>
+                    <div class="l-ayurveda-diagram-dosha">
+                        <p class="l-ayurveda-diagram-dosha-kana">ピッタ</p>
+                        <p class="l-ayurveda-diagram-dosha-english">PITTA</p>
                     </div>
-                    <div>
-                        <p>カパ</p>
-                        <p>KAPHA</p>
+                    <div class="l-ayurveda-diagram-dosha">
+                        <p class="l-ayurveda-diagram-dosha-kana">カパ</p>
+                        <p class="l-ayurveda-diagram-dosha-english">KAPHA</p>
                     </div>
-                    <div>
-                        <p>ヴァータ</p>
-                        <p>VATA</p>
+                    <div class="l-ayurveda-diagram-dosha">
+                        <p class="l-dayurveda-iagram-dosha-kana">ヴァータ</p>
+                        <p class="l-ayurveda-diagram-dosha-english">VATA</p>
                     </div>
                 </div>
-                <div>
-                    <p>アーユルヴェーダでは、食事やヨガ、オイルトリートメント、さらには宝石などの自然の力も使って、さまざまな角度から健康をサポートしていきます。一人ひとりに合った方法で、無理なく自分本来のバランスを取り戻していくのがアーユルヴェーダの魅力です。</p>
-                    <p>いま日本にあるアーユルヴェーダサロンでは、どれか一つの分野に特化していることが多いです。AYUWEDAでは、オールマイティに知識を持つ菊澤がそれぞれの人にあったアプローチをオーダーメイドに執り行います。</p>
-                    <a href="">AYUWEDAのこだわり</a>
+                <div class="l-ayurveda-txt">
+                    <p class="l-ayurveda-txt-item">アーユルヴェーダでは、食事やヨガ、オイルトリートメント、さらには宝石などの自然の力も使って、さまざまな角度から健康をサポートしていきます。一人ひとりに合った方法で、無理なく自分本来のバランスを取り戻していくのがアーユルヴェーダの魅力です。</p>
+                    <p class="l-ayurveda-txt-item">いま日本にあるアーユルヴェーダサロンでは、どれか一つの分野に特化していることが多いです。AYUWEDAでは、オールマイティに知識を持つ菊澤がそれぞれの人にあったアプローチをオーダーメイドに執り行います。</p>
+                    <a class="btn" href="">AYUWEDAのこだわり</a>
                 </div>
             </section>
-            <section id="service">
-                <h4>サービス一覧</h4>
-                <p>AYUWEDAの心と身体を育む3つの柱</p>
-                <div class="p-top-service__list">
-                    <div class="p-top-service__list-item">
-                        <div class="p-top-service__card">
-                            <a class="p-top-service__card-link" href="/service/#totonoeru">
-							    <h5 class="p-top-service__card-heading">整える</h5>
-							    <div class="p-top-service__card-image">
-								    <img src="/assets/images/top/totonoeru.png" class="p-top-service__card-image-item">
-								    <img src="/assets/images/top/totonoeru.png" class="p-top-service__card-image-item">
+            <section id="service" class="l-service">
+                <h4 class="title">サービス一覧</h4>
+                <p class="subTitle">AYUWEDAの心と身体を育む3つの柱</p>
+                <div class="l-service-list">
+                    <div class="l-service-list-item">
+                        <div class="l-service-list-item-card">
+                            <a class="l-service-list-item-card-link" href="/service/#totonoeru">
+							    <h5 class="l-service-list-item-card-link-title">整える</h5>
+							    <div class="l-service-list-item-card-link-img">
+								    <img src="assets/images/top/totonoeru.png">
 							    </div>
-							    <div class="p-top-service__card-body">
-								    <ul class="p-top-service__card-body-lsit">
-									    <li class="p-top-service__card-body-lsit-item">アーユルヴェーダ施術</li>
-									    <li class="p-top-service__card-body-lsit-item">アストロロジー鑑定</li>
-									    <li class="p-top-service__card-body-lsit-item">カウンセリング</li>
-									    <li class="p-top-service__card-body-lsit-item">ヨガ・瞑想</li>
+							    <div class="l-service-list-item-card-link-body">
+								    <ul class="l-service-list-item-card-link-body-list">
+									    <li class="l-service-list-item-card-link-body-list-item">アーユルヴェーダ施術</li>
+									    <li class="l-service-list-item-card-link-body-list-item">アストロロジー鑑定</li>
+									    <li class="l-service-list-item-card-link-body-list-item">カウンセリング</li>
+									    <li class="l-service-list-item-card-link-body-list-item">ヨガ・瞑想</li>
 								    </ul>
 							    </div>
-							    <div class="p-top-service__card-cover"></div>
+							    <div class="l-service-list-item-card-link-body-cover"></div>
 						    </a>
                         </div>
-                        <div class="p-top-service__card">
-                            <a class="p-top-service__card-link" href="/service/#manabu">
-							    <h5 class="p-top-service__card-heading">学ぶ</h5>
-							    <div class="p-top-service__card-image c-thumb">
-								    <img src="/assets/images/top/manabu.png" class="p-top-service__card-image-item">
-								    <img src="/assets/images/top/manabu.png" class="p-top-service__card-image-item">
+                        <div class="l-service-list-item-card">
+                            <a class="l-service-list-item-card-link" href="/service/#manabu">
+							    <h5 class="l-service-list-item-card-link-title">学ぶ</h5>
+							    <div class="l-service-list-item-card-link-img">
+								    <img src="assets/images/top/manabu.png">
 							    </div>
-							    <div class="p-top-service__card-body">
-								    <ul class="p-top-service__card-body-lsit">
-									    <li class="p-top-service__card-body-lsit-item">オンライン講座</li>
-									    <li class="p-top-service__card-body-lsit-item">プログラム</li>
-									    <li class="p-top-service__card-body-lsit-item">アーユルキッチン</li>
-									    <li class="p-top-service__card-body-lsit-item">アーユルヴェーダの図書室</li>
+							    <div class="l-service-list-item-card-link-body">
+								    <ul class="l-service-list-item-card-link-body-list">
+									    <li class="l-service-list-item-card-link-body-list-item">オンライン講座</li>
+									    <li class="l-service-list-item-card-link-body-list-item">プログラム</li>
+									    <li class="l-service-list-item-card-link-body-list-item">アーユルキッチン</li>
+									    <li class="l-service-list-item-card-link-body-list-item">アーユルヴェーダの図書室</li>
 								    </ul>
 							    </div>
-							    <div class="p-top-service__card-cover"></div>
+							    <div class="l-service-list-item-card-link-body-cover"></div>
 						    </a>
                         </div>
-                        <div class="p-top-service__card">
-                            <a class="p-top-service__card-link" href="/service/#toriireru">
-							    <h5 class="p-top-service__card-heading">取り入れる</h5>
-							    <div class="p-top-service__card-image c-thumb">
-								    <img src="/assets/images/top/toriireru.png" class="p-top-service__card-image-item">
-								    <img src="/assets/images/top/toriireru.png" class="p-top-service__card-image-item">
+                        <div class="l-service-list-item-card">
+                            <a class="l-service-list-item-card-link" href="/service/#toriireru">
+							    <h5 class="l-service-list-item-card-link-title">取り入れる</h5>
+							    <div class="l-service-list-item-card-link-img">
+								    <img src="assets/images/top/toriireru.png">
 							    </div>
-							    <div class="p-top-service__card-body">
-								    <ul class="p-top-service__card-body-lsit">
-									    <li class="p-top-service__card-body-lsit-item">食のセルフケア</li>
-									    <li class="p-top-service__card-body-lsit-item">オリジナルグッズ</li>
-									    <li class="p-top-service__card-body-lsit-item">季節のおすすめ</li>
+							    <div class="l-service-list-item-card-link-body">
+								    <ul class="l-service-list-item-card-link-body-list">
+									    <li class="l-service-list-item-card-link-body-list-item">食のセルフケア</li>
+									    <li class="l-service-list-item-card-link-body-list-item">オリジナルグッズ</li>
+									    <li class="l-service-list-item-card-link-body-list-item">季節のおすすめ</li>
 								    </ul>
 							    </div>
-							    <div class="p-top-service__card-cover"></div>
+							    <div class="l-service-list-item-card-link-body-cover"></div>
 						    </a>
                         </div>
                     </div>
                 </div>
-                <a href="">サービス一覧をみる</a>
+                <a class="btn" href="">サービス一覧をみる</a>
             </section>
-            <section id="message">
-                <h4>代表からのメッセージ</h4>
-                <img src="/assets/images/top/profile.png" alt="">
-                <div>
-                    <p>菊澤 理恵</p>
-                    <p>アーユルヴェーダ　トータルプロデュース『AYUWEDA』　代表</p>
-                    <p>自分自身を深く見つめ、日々の暮らしを整えていくアーユルヴェーダの智慧。AYUWEDAでは、その実践をサポートし、あなたの内側からの調和を育むお手伝いをしています。日本の風土や文化にアーユルヴェーダの知恵を融合させ、自分を整える力を育むためのサポートを提供しています。</p>
-                    <a href="">詳しくみる</a>
+            <section id="message" class="l-message">
+                <h4 class="title">代表からのメッセージ</h4>
+                <img src="ayuweda/assets/images/top/profile.png" alt="">
+                <div class="l-message-txt">
+                    <p class="l-message-txt-name">菊澤 理恵</p>
+                    <p class="l-message-txt-position">アーユルヴェーダ　トータルプロデュース『AYUWEDA』　代表</p>
+                    <p class="l-message-txt-item">自分自身を深く見つめ、日々の暮らしを整えていくアーユルヴェーダの智慧。AYUWEDAでは、その実践をサポートし、あなたの内側からの調和を育むお手伝いをしています。日本の風土や文化にアーユルヴェーダの知恵を融合させ、自分を整える力を育むためのサポートを提供しています。</p>
+                    <a class="btn" href="">詳しくみる</a>
                 </div>
             </section>
-            <section id="voice">
-                <h4>お客さまの声</h4>
-                <ul>
-                    <li>
-                        <div>
+            <section id="voice" class="l-voice">
+                <h4 class="title">お客さまの声</h4>
+                <ul class="l-voice-body">
+                    <li class="l-voice-body-list"> 
+                        <div class="l-voice-body-list-item">
                             <img src="" alt="">
-                            <p>52歳　女性（会社員）</p>
+                            <p class="l-voice-body-list-item-name">52歳　女性（会社員）</p>
                         </div>
-                        <p>スリランカの紅茶が好きなのをきっかけに、スリランカで根付いているアーユルヴェーダの施術を受けようと思いました。オールハンド、天然の良質なオイルを使っての施術なので、ケミカルな要素がない所に安心感があります。施術後はむくみが減ってリンパの流れが良くなります。</p>
+                        <p class="l-voice-body-list-txt">スリランカの紅茶が好きなのをきっかけに、スリランカで根付いているアーユルヴェーダの施術を受けようと思いました。オールハンド、天然の良質なオイルを使っての施術なので、ケミカルな要素がない所に安心感があります。施術後はむくみが減ってリンパの流れが良くなります。</p>
                     </li>
-                    <li>
-                        <div>
+                    <li class="l-voice-body-list">
+                        <div class="l-voice-body-list-item">
                             <img src="" alt="">
-                            <p>43歳　女性（主婦）</p>
+                            <p class="l-voice-body-list-item-name">43歳　女性（主婦）</p>
                         </div>
-                        <p>精神的な安定と自分軸を持ちたいという気持ちがありアーユルヴェーダについてなんとなく気になっていたところ、インスタグラムで菊澤さんを知りました。歴史の長さとナチュラルなところに安心感を抱いています。自分との関係性とまわりとの関係性も良くなり、生きやすくなる健康法だと思います。</p>
+                        <p class="l-voice-body-list-txt">精神的な安定と自分軸を持ちたいという気持ちがありアーユルヴェーダについてなんとなく気になっていたところ、インスタグラムで菊澤さんを知りました。歴史の長さとナチュラルなところに安心感を抱いています。自分との関係性とまわりとの関係性も良くなり、生きやすくなる健康法だと思います。</p>
                     </li>
-                    <li>
-                        <div>
+                    <li class="l-voice-body-list">
+                        <div class="l-voice-body-list-item">
                             <img src="" alt="">
-                            <p>55歳　女性（事務職）</p>
+                            <p class="l-voice-body-list-item-name">55歳　女性（事務職）</p>
                         </div>
-                        <p>癌を発症して健康への意識がかわり生活習慣をアーユルヴェーダの実践に変更したのがきっかけです。アーユルヴェーダは範囲が広く難しい部分もありますが、菊澤さんのアドバイスのもと、少しずつ取り入れ自然と馴染んできてるように思います。予防法セルフケアが自分でできるところが好きです。</p>
+                        <p class="l-voice-body-list-txt">癌を発症して健康への意識がかわり生活習慣をアーユルヴェーダの実践に変更したのがきっかけです。アーユルヴェーダは範囲が広く難しい部分もありますが、菊澤さんのアドバイスのもと、少しずつ取り入れ自然と馴染んできてるように思います。予防法セルフケアが自分でできるところが好きです。</p>
                     </li>
                 </ul>                    
             </section>
-            <section id="news">
-                <h4>お知らせ</h4>
-                <dl>
-                    <dt>
-                        <time>2025.05.03</time>
-                        <span>お知らせ</span>
+            <section id="news" class="l-news">
+                <h4 class="title">お知らせ</h4>
+                <dl class="l-news-list">
+                    <dt class="l-news-list-item">
+                        <time class="l-news-list-item-date">2025.05.03</time>
+                        <span class="l-news-list-item-txt">お知らせ</span>
                     </dt>
-                    <dd>めぐる季節とアーユルヴェーダの暮らし開催のお知らせ</dd>
-                    <dt>
-                        <time>2025.04.24</time>
-                        <span>お知らせ</span>
+                    <dd class="l-news-list-txt">めぐる季節とアーユルヴェーダの暮らし開催のお知らせ</dd>
+                    <dt class="l-news-list-item">
+                        <time class="l-news-list-item-date">2025.04.24</time>
+                        <span class="l-news-list-item-txt">お知らせ</span>
                     </dt>
-                    <dd>Ayurveda Program第3回目開催のお知らせ</dd>
-                    <dt>
-                        <time>2025.04.13</time>
-                        <span>お知らせ</span>
+                    <dd class="l-news-list-txt">Ayurveda Program第3回目開催のお知らせ</dd>
+                    <dt class="l-news-list-item">
+                        <time class="l-news-list-item-date">2025.04.13</time>
+                        <span class="l-news-list-item-txt">お知らせ</span>
                     </dt>
-                    <dd>アーユルキッチン「曼荼羅ごはん教室」開催のお知らせ</dd>
+                    <dd class="l-news-list-txt">アーユルキッチン「曼荼羅ごはん教室」開催のお知らせ</dd>
                 </dl>
+                <a class="btn" href="">もっとみる</a>
             </section>
-            <section id="column">
-                <h4>コラム</h4>
+            <section id="column" class="l-column">
+                <h4 class="title">コラム</h4>
 
 
 
             </section>
-            <section id="faq">
-                <h4>よくあるご質問</h4>
-                <ul>
-                    <li><p>Q</p>アーユルヴェーダとはなんですか？</li>
-                    <li><p>Q</p>料金はどのような感じになりますか？</li>
-                    <li><p>Q</p>どのサービスから受けるのがいいですか？</li>
-                    <li><p>Q</p>体験コースはありますか？</li>
-                    <li><p>Q</p>アーユルヴェーダは安全ですか？</li>
-                    <li><p>Q</p>勧誘はありませんか？</li>
+            <section id="faq" class="l-faq">
+                <h4 class="title">よくあるご質問</h4>
+                <ul class="l-faq-list">
+                    <li class="l-faq-list-item"><p class="l-faq-list-item-q">Q</p>アーユルヴェーダとはなんですか？</li>
+                    <li class="l-faq-list-item"><p class="l-faq-list-item-q">Q</p>料金はどのような感じになりますか？</li>
+                    <li class="l-faq-list-item"><p class="l-faq-list-item-q">Q</p>どのサービスから受けるのがいいですか？</li>
+                    <li class="l-faq-list-item"><p class="l-faq-list-item-q">Q</p>体験コースはありますか？</li>
+                    <li class="l-faq-list-item"><p class="l-faq-list-item-q">Q</p>アーユルヴェーダは安全ですか？</li>
+                    <li class="l-faq-list-item"><p class="l-faq-list-item-q">Q</p>勧誘はありませんか？</li>
                 </ul>
-                <a href="">お問い合わせはこちら</a>
+                <a class="btn" href="">お問い合わせはこちら</a>
             </section>
         </article>
     </main>
